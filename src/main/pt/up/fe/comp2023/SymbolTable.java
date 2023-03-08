fecc85c189d9b04a21151a6acbaf6a1177484d09
Diogo Nunes <diogoffn@gmail.com>
2023-03-08 17:37:44 +0000

    added base SymbolTable

--- src/main/pt/up/fe/comp2023/SymbolTable.java
+++ src/main/pt/up/fe/comp2023/SymbolTable.java
@@ -1,13 +1,60 @@
 package pt.up.fe.comp2023;
 
-import pt.up.fe.comp.jmm.analysis.JmmAnalysis;
-import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
-import pt.up.fe.comp.jmm.parser.JmmParserResult;
+import pt.up.fe.comp.jmm.analysis.table.Symbol;
+import pt.up.fe.comp.jmm.analysis.table.Type;
+import pt.up.fe.comp.jmm.report.Report;
 
-public class SymbolTable implements JmmAnalysis {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SymbolTable implements pt.up.fe.comp.jmm.analysis.table.SymbolTable {
+    private List<Report> reports; // do i need this??
+    private  String SuperClassName;
+    private String className;
+    private final List<String> imports = new ArrayList<>();
+    private final Map<String, Symbol> fields = new HashMap<>();
+    private final Map<String, JmmMethod> methods = new HashMap<>();
+
 
     @Override
-    public JmmSemanticsResult semanticAnalysis(JmmParserResult jmmParserResult) {
+    public List<String> getImports() {
+        return imports;
+    }
+
+    @Override
+    public String getClassName() {
+        return className;
+    }
+
+    @Override
+    public String getSuper() {
+        return SuperClassName;
+    }
+
+    @Override
+    public List<Symbol> getFields() {
+        return new ArrayList<>(fields.values());
+    }
+
+    @Override
+    public List<String> getMethods() {
+        return new ArrayList<>(methods.keySet());
+    }
+
+    @Override
+    public Type getReturnType(String s) {
+        return null;
+    }
+
+    @Override
+    public List<Symbol> getParameters(String s) {
+        return null;
+    }
+
+    @Override
+    public List<Symbol> getLocalVariables(String s) {
         return null;
     }
 }
