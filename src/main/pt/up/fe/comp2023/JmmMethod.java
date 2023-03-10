aa36952c3a01e877b09356d08a00fe6dee2a95f1
Diogo Nunes <diogoffn@gmail.com>
2023-03-10 09:35:18 +0000

    added vars and constructor to JmmMethod

--- src/main/pt/up/fe/comp2023/JmmMethod.java
+++ src/main/pt/up/fe/comp2023/JmmMethod.java
@@ -1,4 +1,23 @@
 package pt.up.fe.comp2023;
 
+import pt.up.fe.comp.jmm.analysis.table.Symbol;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class JmmMethod {
+    private String methodName;
+    private Type returnType;
+    private List<Symbol> parameters;
+    private final Map<String,Symbol> localVars = new HashMap<>();
+
+    public JmmMethod(String methodName, Type returnType, List<Symbol> parameters){
+        this.methodName = methodName;
+        this.returnType = returnType;
+        this.parameters = parameters;
+        for(Symbol parameter : parameters)
+            localVars.put(parameter.getName(),parameter);
+    }
 }
