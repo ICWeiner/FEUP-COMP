822967292256d9fffd663bbb4a2525a7f5fde81e
up202005801 <up202005801@fe.up.pt>
2023-03-23 11:28:38 +0000

    Added Some Instructions

--- src/main/pt/up/fe/comp2023/JasminGenerator.java
+++ src/main/pt/up/fe/comp2023/JasminGenerator.java
@@ -5,10 +5,13 @@ import org.specs.comp.ollir.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.specs.comp.ollir.OperationType.*;
+
 public class JasminGenerator {
     private ClassUnit classUnit;
     private int CounterStack;
     private int CounterMax;
+    private int ConditionInt;
 
     public JasminGenerator(ClassUnit classUnit) {
         this.classUnit = classUnit;
@@ -71,4 +74,122 @@ public class JasminGenerator {
         BuilderOfStrings.append("\n.end method\n");
         return BuilderOfStrings.toString();
     }
+
+    private String dealWithInstruction(Instruction instruction, HashMap<String, Descriptor> varTable, HashMap<String, Instruction> labels) {
+        StringBuilder BuilderOfStrings = new StringBuilder();
+        for (Map.Entry<String, Instruction> entry : labels.entrySet()) {
+            if (entry.getValue().equals(instruction)) {
+                BuilderOfStrings.append(entry.getKey()).append(":\n");
+            }
+        }
+        return switch (instruction.getInstType()) {
+            case ASSIGN ->
+                    BuilderOfStrings.append(dealWithAssignment((AssignInstruction) instruction, varTable)).toString();
+            case NOPER ->
+                    BuilderOfStrings.append(dealWithSingleOpInstruction((SingleOpInstruction) instruction, varTable)).toString();
+            case BINARYOPER ->
+                    BuilderOfStrings.append(dealWithBinaryOpInstruction((BinaryOpInstruction) instruction, varTable)).toString();
+            case UNARYOPER -> "Deal with '!' in correct form";
+            default -> "Error in Instructions";
+        };
+    }
+
+    private String dealWithBinaryOpInstruction(BinaryOpInstruction instruction, HashMap<String, Descriptor> varTable) {
+        switch (instruction.getUnaryOperation().getOpType()) {
+            case ADD:
+            case SUB:
+            case MUL:
+            case DIV:
+                return this.dealWithIntOperation(instruction, varTable);
+            case LTH:
+            case GTE:
+            case ANDB:
+            case NOTB:
+                return this.dealWithBooleanOperation(instruction, varTable);
+            default:
+                return "Error in BinaryOpInstruction";
+        }
+    }
+
+    private String dealWithBooleanOperation(BinaryOpInstruction instruction, HashMap<String, Descriptor> varTable) {
+        OperationType ot = instruction.getUnaryOperation().getOpType();
+        StringBuilder BuilderOfStrings = new StringBuilder();
+        switch (instruction.getUnaryOperation().getOpType()) {
+            case LTH, GTE -> {
+                // ..., value1, value2 →
+                // ...
+                String leftOperand = loadElement(instruction.getLeftOperand(), varTable);
+                String rightOperand = loadElement(instruction.getRightOperand(), varTable);
+                BuilderOfStrings.append(leftOperand)
+                        .append(rightOperand)
+                        .append(this.dealWithRelationalOperation(ot, this.getTrueLabel()))
+                        .append("iconst_1\n")
+                        .append("goto ").append(this.getEndIfLabel()).append("\n")
+                        .append(this.getTrueLabel()).append(":\n")
+                        .append("iconst_0\n")
+                        .append(this.getEndIfLabel()).append(":\n");
+                // if_icmp decrements 2, iconst increments 1
+                this.decrementStackCounter(1);
+            }
+            case ANDB -> {
+                // ..., value →
+                // ...
+                String ifeq = "ifeq " + this.getTrueLabel() + "\n";
+                // Compare left operand
+                BuilderOfStrings.append(loadElement(instruction.getLeftOperand(), varTable)).append(ifeq);
+                this.decrementStackCounter(1);
+                // Compare right operand
+                BuilderOfStrings.append(loadElement(instruction.getRightOperand(), varTable)).append(ifeq);
+                this.decrementStackCounter(1);
+                BuilderOfStrings.append("iconst_1\n")
+                        .append("goto ").append(this.getEndIfLabel()).append("\n")
+                        .append(this.getTrueLabel()).append(":\n")
+                        .append("iconst_0\n")
+                        .append(this.getEndIfLabel()).append(":\n");
+                // iconst
+                this.incrementStackCounter(1);
+            }
+            case NOTB -> {
+                String operand = loadElement(instruction.getLeftOperand(), varTable);
+                BuilderOfStrings.append(operand)
+                        .append("ifne ").append(this.getTrueLabel()).append("\n")
+                        .append("iconst_1\n")
+                        .append("goto ").append(this.getEndIfLabel()).append("\n")
+                        .append(this.getTrueLabel()).append(":\n")
+                        .append("iconst_0\n")
+                        .append(this.getEndIfLabel()).append(":\n");
+                // No need to change stack, load increments 1, ifne would dec.1 and iconst would inc.1
+            }
+            default -> {
+                return "Error in BooleansOperations\n";
+            }
+        }
+        this.ConditionInt++;
+        return BuilderOfStrings.toString();
+    }
+
+    private String dealWithIntOperation(BinaryOpInstruction instruction, HashMap<String, Descriptor> varTable) {
+        String leftOperand = loadElement(instruction.getLeftOperand(), varTable);
+        String rightOperand = loadElement(instruction.getRightOperand(), varTable);
+        String operation;
+        switch (instruction.getUnaryOperation().getOpType()) {
+            // ..., value1, value2 →
+            // ..., result
+            case ADD -> operation = "iadd\n";
+            case SUB -> operation = "isub\n";
+            case MUL -> operation = "imul\n";
+            case DIV -> operation = "idiv\n";
+            default -> {
+                return "Error in IntOperation\n";
+            }
+        }
+        this.decrementStackCounter(1);
+        return leftOperand + rightOperand + operation;
+    }
+
+
+    private String dealWithSingleOpInstruction(SingleOpInstruction instruction, HashMap<String, Descriptor> varTable) {
+        return loadElement(instruction.getSingleOperand(), varTable);
+    }
 }
+
