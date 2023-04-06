e0f6ac47343be8e99361791c5be1b3ce6710f2ac
Diogo Nunes <diogoffn@gmail.com>
2023-04-06 04:01:35 -0700

    commented out code unrelated to my branch

--- src/main/pt/up/fe/comp2023/ast/JmmAnalyser.java
+++ src/main/pt/up/fe/comp2023/ast/JmmAnalyser.java
@@ -42,6 +42,7 @@ public class JmmAnalyser implements JmmAnalysis {
         System.out.println("Symbol Table Filled!");
 
         System.out.println(table);
+        /*
 
         System.out.println("Visitor - Semantic Analysis...");
 
@@ -52,6 +53,8 @@ public class JmmAnalyser implements JmmAnalysis {
         System.out.println("Semantic Analysis Done!");
         //if(!reports.isEmpty())
             System.out.println(reports);
+
+         */
 
         return new JmmSemanticsResult(jmmParserResult, table, reports);
     }
