98a3220e6b5050f593a4b9a49361766d8fa134c6
Daniela Tomás <daniela.stomas02@gmail.com>
2023-04-20 11:07:42 +0100

    trying to fix array index

--- src/main/pt/up/fe/comp2023/ast/SemanticAnalysisVisitor.java
+++ src/main/pt/up/fe/comp2023/ast/SemanticAnalysisVisitor.java
@@ -271,8 +271,8 @@ public class SemanticAnalysisVisitor extends AJmmVisitor<Boolean, Boolean> {
         String className = table.getClassName();
         if(!child.getKind().equals("Identifier")) {
             //TODO é provavel que estas condições não estejam bem 💀
-            if(!(nodeType.isArray() && nodeType.getName().equals("int") && child.getKind().equals("IntArrayDeclaration") && child.getJmmChild(0).getKind().equals("Integer"))
-                    && !(!nodeType.isArray() && nodeType.getName().equals("int") && child.getKind().equals("Integer"))
+            if(//!(nodeType.isArray() && nodeType.getName().equals("int") && child.getKind().equals("IntArrayDeclaration") && child.getJmmChild(0).getKind().equals("Integer"))
+                    !(!nodeType.isArray() && nodeType.getName().equals("int") && child.getKind().equals("Integer"))
                     && !(child.getKind().equals("Boolean") && nodeType.getName().equals("boolean"))
                     && !(child.getKind().equals("GeneralDeclaration") && nodeType.getName().equals(child.get("name"))) //TODO acho que isto não está bem
                     && !((child.getKind().equals("BinaryOp") && (child.get("op").equals("&&") && nodeType.getName().equalsIgnoreCase("boolean") || (!child.get("op").equals("&&") && nodeType.getName().equals("int"))) && visit(child,true))) //TODO isto dá dois reports
