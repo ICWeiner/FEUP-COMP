5d3c5b4c11eb17581f97b239b71dd9b26d47c956
Daniela Tomás <daniela.stomas02@gmail.com>
2023-04-22 12:05:25 +0100

    fix array index good

--- src/main/pt/up/fe/comp2023/ast/SemanticAnalysisVisitor.java
+++ src/main/pt/up/fe/comp2023/ast/SemanticAnalysisVisitor.java
@@ -265,6 +265,13 @@ public class SemanticAnalysisVisitor extends AJmmVisitor<Boolean, Boolean> {
             if(reports.isEmpty()) reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(node.get("lineStart")), Integer.parseInt(node.get("colStart")), "Error: Array index is not an integer"));
             return false;
         }
+        else if(leftChild.getKind().equals("Identifier")) {
+            Type leftChildType = table.getVariableType(leftChild.get("value"),currentMethodName);
+            if(leftChildType == null || !leftChildType.getName().equals("int")) {
+                reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(node.get("lineStart")), Integer.parseInt(node.get("colStart")), "Error: Array index is not an integer"));
+                return false;
+            }
+        }
         else if(leftChild.getKind().equals("MethodCall") && !table.getImports().contains(leftChild.getJmmChild(0).get("value")) && !table.getReturnType(currentMethodName).getName().equals("int") && !visit(leftChild,true)) { //TODO
             if(reports.isEmpty()) reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(node.get("lineStart")), Integer.parseInt(node.get("colStart")), "Error: Array index is not an integer"));
             return false;
