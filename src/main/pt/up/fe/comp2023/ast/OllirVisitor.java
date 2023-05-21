4ce51a810e3a1cd51f57b5803d497192be45aaee
Diogo Nunes <diogoffn@gmail.com>
2023-05-21 20:40:33 +0100

    OLLIR bug fix

--- src/main/pt/up/fe/comp2023/ast/OllirVisitor.java
+++ src/main/pt/up/fe/comp2023/ast/OllirVisitor.java
@@ -705,12 +705,13 @@ public class OllirVisitor extends AJmmVisitor<List<Object>, List<Object>> {
                         System.out.println("ENTREI NO 3");
                         ollirExpression = OllirTemplates.invokevirtual(OllirTemplates.variable((Symbol) targetReturn.get(1)), params.getValue(), expectedType,  params.getValue());
                     }
+                }else if(method == null) {
+                    Symbol targetVariable = (Symbol) targetReturn.get(1);
+                    ollirExpression = OllirTemplates.invokevirtual( node.get("value"), assignment.getType(),  params.getValue());//OllirTemplates.invokevirtual(OllirTemplates.variable(targetVariable), method.getName(), method.getReturnType(), params.getValue());
+                    //expectedType = assignment.getType();
+                    expectedType = (expectedType == null) ? new Type("void", false) : expectedType;
                 } else if (!methodClass.equals("length")) {
                     Symbol targetVariable = (Symbol) targetReturn.get(1);
-                    System.out.println("ENTREI NO 4");
-                    System.out.println("method.getName() :" + method.getName());
-                    System.out.println("method.getReturnType() :" + method.getReturnType());
-                    System.out.println("params.getValue() :" + params.getValue());
                     ollirExpression = OllirTemplates.invokevirtual(OllirTemplates.variable(targetVariable), method.getName(), method.getReturnType(), params.getValue());
                     expectedType = method.getReturnType();
                 }else {
